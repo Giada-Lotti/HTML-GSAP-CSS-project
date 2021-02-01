e49c001f31218efe948b04f7a7d85d03377f9a5b
Giada Lotti (Desdemona) <61546865+Giada-Lotti@users.noreply.github.com>
2021-02-02 00:48:10 +0100

    Add files via upload

---
+++ fashionWebsite/main.js
@@ -0,0 +1,7 @@
+gsap.from (".main-links ul li",0.5,{opacity:0,stagger:0.5, ease: "ease"} )
+gsap.from (".logo",0.8,{opacity:0,ease: "power.out"} )
+gsap.from (".main-01",2.6,{ ease: "expo.out", x: 500 })
+gsap.from (".main-02",2.6,{ ease: "expo.out", x: 500 })
+gsap.from(".text h1",2, {opacity:0,ease: "power2.out", y: -500 });
+gsap.from(".text p",2, {opacity:0,ease: "power2.out",delay:0.8});
+
